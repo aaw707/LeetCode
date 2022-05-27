c51bc5687f9fa97dcb61898095a2bde25d318b57
Angel <angel.wang707@gmail.com>
2022-05-26 21:10:38 -0700

    Create 1342. Number of Steps to Reduce a Number to Zero.py

---
+++ 1342. Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        res = 0
+        
+        # simulation
+        while num > 0:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+            res += 1
+            
+        return res
