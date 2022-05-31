92c5a80414cb5d234e0f147146755f6605254c5a
Angel <angel.wang707@gmail.com>
2022-05-31 15:48:12 -0700

    Create 148. Sort List.py

---
+++ 148. Sort List.py
@@ -0,0 +1,110 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    
+    
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        # def toString(head):
+        #     res = "[ "
+        #     while head:
+        #         res += str(head.val)
+        #         res += " "
+        #         head = head.next
+        #     res += "]"
+        #     return res     
+        
+        # base case: no node or 1 node
+        # print("head:", toString(head))
+        if not head or not head.next:
+            return head
+        
+        # divide LL into 2 unsorted queues
+        Q1 = ListNode(-1)
+        Q2 = ListNode(-1)
+        
+        P1 = Q1
+        P2 = Q2 
+        
+        node = head
+        while node:
+            P1.next = node
+            P1 = P1.next 
+            node = node.next 
+            if node:
+                P2.next = node
+                P2 = P2.next
+                node = node.next 
+        
+        P1.next = None
+        P2.next = None
+        
+        Q1 = Q1.next 
+        Q2 = Q2.next 
+        
+        # print("Q1:", toString(Q1))
+        # print("Q2:", toString(Q2))
+        # sort 2 queues separately
+        Q1_sorted = self.sortList(Q1)
+        Q2_sorted = self.sortList(Q2)
+        # print("Q1_sorted:", toString(Q1_sorted))
+        # print("Q2_sorted:", toString(Q2_sorted))
+        
+        # merge Q1 and Q2
+        new_head = ListNode(-1)
+        node = new_head
+        
+        while Q1_sorted and Q2_sorted:
+            if Q1_sorted.val < Q2_sorted.val:
+                node.next = Q1_sorted
+                Q1_sorted = Q1_sorted.next 
+                node = node.next 
+            else: # Q1_sorted.val >= Q2_sorted.val
+                node.next = Q2_sorted
+                Q2_sorted = Q2_sorted.next 
+                node = node.next 
+                
+        
+        # print("new_head after initial merge:", toString(new_head)        )
+        # check for remaining Q1/Q2
+        if Q1_sorted:
+            node.next = Q1_sorted
+        if Q2_sorted:
+            node.next = Q2_sorted
+        
+        # print("new_head after all merge:", toString(new_head))
+        return new_head.next
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
